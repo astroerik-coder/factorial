4c58bf758edda8eeaffbe458d0e5f938f1121a8b
astroerik <85704293+astroerik-coder@users.noreply.github.com>
2025-05-13 10:01:37 -0500

    [DEL] Build

--- build/CMakeFiles/FactorialApp.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for FactorialApp.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/GitHub/factorial
-# Build directory: D:/GitHub/factorial/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("D:/GitHub/factorial/build/UnitTests[1]_include.cmake")
-subdirs("_deps/googletest-build")
